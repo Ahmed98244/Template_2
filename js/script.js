1ce86041cffef0f96c32eec2c0c663047b14cee3
Ahmed98244 <ahmedmartex90@gmail.com>
2021-10-16 22:22:05 +0200

    New features

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*global $, alert, console*/
+$(function () {
+  "use strict";
+
+  // add nice scroll
+  $("body").niceScroll({
+    cursorcolor: "#19c8fa",
+    cursorwidth: "10px",
+    cursorborder: "1px solid #1abc9c",
+    zindex: "5",
+  });
+});
